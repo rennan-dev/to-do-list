a3dfdc03ad5ac6381aea6ee9b014f52a34293aa2
Rennan-Dev <rennandev7@gmail.com>
2025-03-12 10:27:46 -0400

    ♻️ refactor: codigo ajustado.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,26 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
+import './App.css';
 
 function Home() {
   const location = useLocation();
+  const navigate = useNavigate();
   const nome = new URLSearchParams(location.search).get('nome');
 
+  const handleLogout = async () => {
+    const response = await fetch('', {
+      method: 'GET',
+    });
+
+    if(response.ok) {
+      navigate('/login');
+    }
+  };
+
   return (
-    <div>
+    <div className="container">
       <h1>Bem-vindo, {nome}!</h1>
+      <button className="logout" onClick={handleLogout}>Logout</button>
     </div>
   );
 }
